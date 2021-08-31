95b35547c41db09e0cfd87979fd789cc66597008
kotreshrnaik <87847893+kotreshrnaik@users.noreply.github.com>
2021-08-31 13:48:52 +0530

    Initial Submit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+use \CodeIgniter\Controller;
+
+class Home extends Controller
+{
+	public function index()
+	{
+		echo "Welcome to A2Z ENGLISH TIPS";
+                /*return view('welcome_message');*/
+	}
+}
