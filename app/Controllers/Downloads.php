123dbd1d15d8e12b47b3ea6c5a6cb22e80060b02
kotreshrnaik <87847893+kotreshrnaik@users.noreply.github.com>
2021-09-03 13:07:39 +0530

    created controllers

---
+++ app/Controllers/Downloads.php
@@ -0,0 +1,53 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace App\Controllers;
+use CodeIgniter\Controller;
+/**
+ * Description of DesignSolutions
+ *
+ * @author Admin
+ */
+class Downloads extends Controller{
+    public function index()
+    {
+        return view('home_view');
+    }
+    public function grammar()
+    {
+        return view('downloads/grammar_view');
+    }
+    public function SpokenEnglish()
+    {
+        return view('downloads/spokenenglish_view');
+    }
+    public function PartsOfSpeech()
+    {
+        return view('downloads/partsofspeech_view');
+    }
+    public function Vocabulary()
+    {
+        return view('downloads/vocabulary_view');
+    }
+    public function Translation()
+    {
+        return view('downloads/translation_view');
+    }
+    public function ObjectiveEnglish()
+    {
+        return view('downloads/objectiveenglish_view');
+    }
+    public function InterviewQandA()
+    {
+        return view('downloads/interview_view');
+    }
+    public function Essays()
+    {
+        return view('downloads/essays_view');
+    }
+    
+}
