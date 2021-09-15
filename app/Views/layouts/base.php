40905fccea99336016d4c88fce862b624376cf8d
kotreshrnaik <87847893+kotreshrnaik@users.noreply.github.com>
2021-09-15 18:14:34 +0530

    updated logos

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -4,7 +4,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="<?= base_url();?>/public/assets/images/favicon.png" type="image/gif" >
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -194,7 +194,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-3">
-                        <div class="text-center"><img src="<?= base_url(); ?>/public/assets/images/logo2.png" alt="" class="img-fluid"></div>
+                        <div class="text-center"><img src="<?= base_url(); ?>/public/assets/images/logo4.png" alt="" class="img-fluid"></div>
                         <address class="text-center"> 
                             #485, 10th Cross, 8th Main, 
                             HAL 3rd Stage, Jeevan Bhima Nagar,
