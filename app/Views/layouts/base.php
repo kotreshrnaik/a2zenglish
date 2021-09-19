f2354c8723994ac6fce6e58bc2e9b020acf9f1ee
kotreshrnaik <87847893+kotreshrnaik@users.noreply.github.com>
2021-09-20 00:24:28 +0530

    Updated base

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -249,8 +249,8 @@
         </footer>
 
         
-        <!--Start of Tawk.to Script-->
-        <!-- <script type="text/javascript">
+        Start of Tawk.to Script
+         <script type="text/javascript">
         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -260,8 +260,8 @@
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
         })();
-        </script> -->
-        <!--End of Tawk.to Script-->
+        </script> 
+        End of Tawk.to Script
 
         <!-- Optional JavaScript; choose one of the two! -->
 
