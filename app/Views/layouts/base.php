a4e41b93f9e82e6091d19991251639b2fe03ebe4
kotreshrnaik <87847893+kotreshrnaik@users.noreply.github.com>
2021-09-25 10:44:13 +0530

    Added meta title and description

--- app/Views/layouts/base.php
+++ app/Views/layouts/base.php
@@ -9,7 +9,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href ="<?= base_url(); ?>/public/assets/scss/style.css">
-        <title>A2Z ENGLISH TIPS</title>
+        <title>Learn English Online | English Grammar | Spoken English | Jobs Skills</title>
+        <meta name="description" content="Learn English Online with the free learning English videos and materials from A2Z ENGLISH TIPS. This channel will help you to learn English and improve your Pronunciation, Grammar, Spoken English, Communication Skills, Interview Skills, Life Skills, Motivations and vocabulary knowledge.">
+
     </head>
     <body>
         <!-- top-header start -->
